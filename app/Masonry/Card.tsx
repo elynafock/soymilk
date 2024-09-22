b9d40ed7700d37bc0fc30f2d31616d7b12c25f4c
Elyna Fock <elinayen2000@gmail.com>
2024-09-22 13:44:59 +0800

    masonry algo done

--- app/Masonry/Card.tsx
+++ app/Masonry/Card.tsx
@@ -7,9 +7,12 @@ type Props = {
 
 const Card = (props: Props) => {
   return (
-    <div className="p-2 bg-green-700">
-      <div className="w-[314px] h-[368px] bg-green-300"></div>
-      <div className="w-[314px]">
+    <div className="p-2 bg-green-700 rounded-lg ">
+      <div
+        className="w-full bg-green-300"
+        style={{ height: props.post.img.height }}
+      ></div>
+      <div className="w-full">
         <div className="text-xl pt-4 ">{props.post.title}</div>
         <div className="text-base text-[#A1A0A0] pt-3 ">
           {props.post.createdAt.toDateString()}
--- app/Masonry/index.tsx
+++ app/Masonry/index.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import React from "react";
 import Card from "./Card";
+import useMediaQuery from "../hooks";
 
 export type Post = {
   id: string;
@@ -19,7 +22,7 @@ const posts: Array<Post> = [
     img: {
       url: "././public/hw6.png",
       width: 314,
-      height: 368,
+      height: 238,
     },
     createdAt: new Date(2024, 8, 4),
   },
@@ -29,17 +32,56 @@ const posts: Array<Post> = [
     img: {
       url: "././public/hw6.png",
       width: 314,
-      height: 368,
+      height: 349,
+    },
+    createdAt: new Date(2024, 8, 4),
+  },
+  {
+    id: "3",
+    title: "Drawing 3",
+    img: {
+      url: "././public/hw6.png",
+      width: 314,
+      height: 543,
+    },
+    createdAt: new Date(2024, 8, 4),
+  },
+  {
+    id: "4",
+    title: "Drawing 4",
+    img: {
+      url: "././public/hw6.png",
+      width: 314,
+      height: 600,
+    },
+    createdAt: new Date(2024, 8, 4),
+  },
+  {
+    id: "5",
+    title: "Drawing 4",
+    img: {
+      url: "././public/hw6.png",
+      width: 314,
+      height: 200,
     },
     createdAt: new Date(2024, 8, 4),
   },
 ];
 
 const Cards = () => {
+  const isDesktop = useMediaQuery("(min-width: 1024px)");
+  const columns = isDesktop ? 3 : 2;
+
   return (
-    <div className="grid grid-cols-3 gap-6 justify-center">
-      {posts.map((post) => (
-        <Card post={post}></Card>
+    <div className="grid grid-cols-2 gap-4 pt-12 px-4 max-w-screen-lg mx-auto lg:grid-cols-3">
+      {[...Array(columns)].map((_, i) => (
+        <div className="flex flex-col gap-4" key={i}>
+          {posts
+            .filter((_, j) => j % columns === i)
+            .map((post) => (
+              <Card post={post} key={post.id} />
+            ))}
+        </div>
       ))}
     </div>
   );
