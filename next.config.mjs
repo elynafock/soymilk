2be3278ef94e3ac4042d955b8beb20a9a298141b
elynafock <73025670+elynafock@users.noreply.github.com>
2025-07-01 15:03:14 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "oongvcbebqpcxrpntpok.supabase.co",
+        hostname: "hhjbneypfxrjmevtwfeq.supabase.co",
         port: "",
       },
     ],
